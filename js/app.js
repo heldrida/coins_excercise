225e7861f81f81c0a4c3dfa7e3f55bf735e4692c
Helder <heldrida@gmail.com>
2014-07-02 22:41:02 +0000

    created properties. add event listener to the form. created method setter for user amount.

--- js/app.js
+++ js/app.js
@@ -2,10 +2,42 @@ document.addEventListener("DOMContentLoaded", function () {
   
 
 	var myApp = {
-		
+
+		myCoins: [2, 1, 0.50, 0.20, 0.02, 0.01],
+
+		myAmount: 0,
+
+		myChange: {},
+
+		myForm: false,
+
+		setAmount: function(x){
+
+			console.log("fn setAmount!");
+
+			this.myAmount = document.userForm.amount.value;;
+
+			console.log(this.myAmount);
+
+		},
+
+		run: function(event){
+			
+			console.log("fn run!");
+
+			event.preventDefault();
+
+			this.setAmount();
+
+		},
+
 		init: function(){
 
-			console.log("My App initialized!");
+			console.log("fn init!");
+
+			this.myForm = document.userForm;
+
+			this.myForm.addEventListener("submit", this.run.bind(this), false);
 
 		}
 
