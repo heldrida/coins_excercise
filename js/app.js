f0cd2bbf2e38eec4eab4a66de7e95337fec9a3cc
Helder <heldrida@gmail.com>
2014-07-02 22:15:46 +0000

    created a basic js module

---
+++ js/app.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  
+
+	var myApp = {
+		
+		init: function(){
+
+			console.log("My App initialized!");
+
+		}
+
+	};
+
+	myApp.init();
+
+
+}, false);
