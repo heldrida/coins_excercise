eaaeb706eeee292e863a35919df0a8e94700e33b
Helder <heldrida@gmail.com>
2014-07-02 23:06:19 +0000

    applied algorithm from prototype I've been testing on http://jsbin.com/zaqumeke/3/ , changing it to work on object literal or module instead

--- js/app.js
+++ js/app.js
@@ -11,6 +11,37 @@ document.addEventListener("DOMContentLoaded", function () {
 
 		myForm: false,
 
+		calc: function(n){
+
+		  if (n === 0){
+		  	
+		  	return false;
+
+		  }
+		  
+		  var self = this;
+		  var num = n < 1 ? n * 100 : n;
+		  this.myChange[n] = [];
+
+		  self.myCoins.forEach(function(v){
+		    
+		    v = n < 1 ? v * 100 : v;
+		    
+		    var x = Math.floor( num / v );
+		    num = num % v;
+
+		    if ( x !== 0 ) {
+		    
+		      self.myChange[n].push([x, v]);
+		    
+		    }
+		    
+		  });
+
+		  console.log(self.myChange);
+
+		},
+
 		normalise: function(usrVal){
 
 			return usrVal === parseInt(usrVal) ? usrVal : usrVal * 100;
@@ -29,24 +60,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
 			ev.preventDefault();
 
-			var x, 
+			var self,
 				integer, 
 				decimal;
+
+			self = this;
 
 			this.setAmount();
 
 			this.myAmount = this.myAmount / 100;
+			
 			integer = Math.floor(this.myAmount);
 			decimal = this.myAmount - integer;
+			decimal = decimal.toFixed(2);
 
-			console.log("this.myAmount");
-			console.log(this.myAmount);
+			[integer, decimal].forEach(function(v){
+				
+				v !== 0 ? self.calc(v) : null;
 
-			console.log("integer:");
-			console.log(integer);
-
-			console.log("decimal");
-			console.log(decimal);
+			});
 
 		},
 
